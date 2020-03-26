e5214c96f1e15d57ce51838e263c1bc85f045b55
Li Zhang <zhangli@us.ibm.com>
2020-03-26 16:44:24 +0100

    add fix for install-helm.sh --version v2.16.3

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -126,7 +126,7 @@ function kube-test-env-up {
     curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > install-helm.sh
     chmod u+x install-helm.sh
     cat install-helm.sh
-    ./install-helm.sh
+    ./install-helm.sh --version v2.16.3
 
     # Start Helm Server
     echo "Installing Helm Server..."
@@ -149,6 +149,7 @@ function kube-test-env-up {
 
     # start mcad controller
     echo "Starting MCAD Controller..."
+    echo "helm install kube-arbitrator namespace kube-system wait set resources.requests.cpu=1000m set resources.requests.memory=1024Mi set resources.limits.cpu=1000m set resources.limits.memory=1024Mi set image.repository=$IMAGE_REPOSITORY_MCAD set image.tag=$IMAGE_TAG_MCAD set image.pullPolicy=$MCAD_IMAGE_PULL_POLICY debug"
     helm install kube-arbitrator --namespace kube-system --wait --set resources.requests.cpu=1000m --set resources.requests.memory=1024Mi --set resources.limits.cpu=1000m --set resources.limits.memory=1024Mi --set image.repository=$IMAGE_REPOSITORY_MCAD --set image.tag=$IMAGE_TAG_MCAD --set image.pullPolicy=$MCAD_IMAGE_PULL_POLICY --debug
 
     sleep 10
