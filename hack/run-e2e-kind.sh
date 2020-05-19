c572c35e8ca5d5f55c9be89491247f31a0592845
Diana Arroyo <darroyo@us.ibm.com>
2020-05-19 09:34:39 -0400

    Debugging e2e timeout failures.
    Signed-off-by: Diana Arroyo <darroyo@us.ibm.com>

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -18,7 +18,8 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubectl
+# todo: FIXME sudo apt-get install -y kubectl
+sudo apt-get install -y  kubectl=1.11.10-00
 
 # Download kind binary (0.2.0)
 sudo curl -o /usr/local/bin/kind -L https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-linux-amd64
