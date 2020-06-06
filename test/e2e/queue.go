bdaf8dbc048e5d55b96d757cec60a6339d135026
Diana Arroyo <darroyo@us.ibm.com>
2020-06-06 10:49:50 -0400

    Fixed syntax for generic pod only test case.
    Signed-off-by: Diana Arroyo <darroyo@us.ibm.com>

--- test/e2e/queue.go
+++ test/e2e/queue.go
@@ -65,7 +65,7 @@ var _ = Describe("Predicates E2E Test", func() {
 	})
 	
 	//NOTE: Recommend this test not to be the last test in the test suite it may pass
-	//      may pass the local test but may cause controller to fail which is not
+	//      the local test but may cause controller to fail which is not
 	//      part of this test's validation.
 
 	It("Create AppWrapper- Bad PodTemplate", func() {
@@ -91,7 +91,7 @@ var _ = Describe("Predicates E2E Test", func() {
 	})
 
 
-	It("Create AppWrapper  - Pod Only - 1 Pod", func() {
+	It("Create AppWrapper  - Generic Pod Only - 1 Pod", func() {
 		context := initTestContext()
 		defer cleanupTestContext(context)
 
@@ -102,7 +102,18 @@ var _ = Describe("Predicates E2E Test", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	It("Create AppWrapper  - Generic PodTemplate Only - 2 Pods", func() {
+	It("Create AppWrapper  - Bad Generic Pod Only - 1 Pod", func() {
+		context := initTestContext()
+		defer cleanupTestContext(context)
+
+		aw := createGenericPodAW(context,"aw-bad-generic-pod-1")
+
+		err := waitAWReady(context, aw)
+
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("Create AppWrapper  - Bad Generic PodTemplate Only - 2 Pods", func() {
 		context := initTestContext()
 		defer cleanupTestContext(context)
 
