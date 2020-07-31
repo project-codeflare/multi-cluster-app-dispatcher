411294fac3d3d12c443950e1de231a3fa70581b6
Diana Arroyo <darroyo@us.ibm.com>
2020-07-31 14:35:27 -0400

    Debugging bursting use case.
    Signed-off-by: Diana Arroyo <darroyo@us.ibm.com>

--- test/e2e/queue.go
+++ test/e2e/queue.go
@@ -27,12 +27,12 @@ import (
 
 var _ = Describe("AppWrapper E2E Test", func() {
 
-	It("Create AppWrapper - Generic 100 Deployment Only - 2 pods each", func() {
+	It("Create AppWrapper - Generic 99 Deployment Only - 2 pods each", func() {
 		context := initTestContext()
 		defer cleanupTestContext(context)
 
 		const (
-			awCount = 100
+			awCount = 99
 		)
 
 		replicas := 2
