1026462bd7c09b8e4adc298f7d746041b0606300
dmatch01 <darroyo@us.ibm.com>
2021-08-23 11:57:54 -0500

    Added updated copyright for quota manager.
    Signed-off-by: dmatch01 <darroyo@us.ibm.com>

--- pkg/controller/quota/quotamanager/quota_rest_manager.go
+++ pkg/controller/quota/quotamanager/quota_rest_manager.go
@@ -1,4 +1,15 @@
 // +build !private
+// ------------------------------------------------------ {COPYRIGHT-TOP} ---
+// IBM Confidential
+// OCO Source Materials
+// IBM Watson Machine Learning Core
+//
+// Copyright IBM Corp. 2021
+//
+// The source code for this program is not published or otherwise
+// divested of its trade secrets, irrespective of what has been
+// deposited with the U.S. Copyright Office.
+// ------------------------------------------------------ {COPYRIGHT-END} ---
 
 package quotamanager
 
