9e7627796f2037d6892e650d2716f9206933b16c
dmatch01 <darroyo@us.ibm.com>
2021-08-24 13:17:41 -0500

    Fix syntax.
    Signed-off-by: dmatch01 <darroyo@us.ibm.com>

--- pkg/controller/metrics/dynamicmapper/mapper.go
+++ pkg/controller/metrics/dynamicmapper/mapper.go
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 /*
- */
 Copyright 2019, 2021 The Multi-Cluster App Dispatcher Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
