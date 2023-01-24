a00598d06cc3fe3e244f389a84c3dd344f06acb8
asm582 <asm582@nyu.edu>
2023-01-24 16:09:37 -0500

    update schedulingspec for runtime feat

--- pkg/apis/controller/v1beta1/schedulingspec.go
+++ pkg/apis/controller/v1beta1/schedulingspec.go
@@ -45,9 +45,37 @@ type SchedulingSpec struct {
 	Spec SchedulingSpecTemplate `json:"spec,omitempty" protobuf:"bytes,1,rep,name=spec"`
 }
 
+type ClusterReference struct {
+	Name string `json:"name"`
+}
+
+type ClusterSchedulingSpec struct {
+	Clusters        []ClusterReference    `json:"clusters,omitempty"`
+	ClusterSelector *metav1.LabelSelector `json:"clusterSelector,omitempty"`
+}
+
+type ScheduleTimeSpec struct {
+	Min     metav1.Time `json:"minTimestamp,omitempty"`
+	Desired metav1.Time `json:"desiredTimestamp,omitempty"`
+	Max     metav1.Time `json:"maxTimestamp,omitempty"`
+}
+
+type RuntimeDurationSpec struct {
+	Expected metav1.Duration `json:"expected,omitempty"`
+	Limit    metav1.Duration `json:"limit,omitempty"`
+}
+
+type DispatchingWindowSpec struct {
+	Start ScheduleTimeSpec `json:"start,omitempty"`
+	End   ScheduleTimeSpec `json:"end,omitempty"`
+}
+
 type SchedulingSpecTemplate struct {
-	NodeSelector map[string]string `json:"nodeSelector,omitempty" protobuf:"bytes,1,rep,name=nodeSelector"`
-	MinAvailable int               `json:"minAvailable,omitempty" protobuf:"bytes,2,rep,name=minAvailable"`
+	NodeSelector      map[string]string     `json:"nodeSelector,omitempty" protobuf:"bytes,1,rep,name=nodeSelector"`
+	MinAvailable      int                   `json:"minAvailable,omitempty" protobuf:"bytes,2,rep,name=minAvailable"`
+	ClusterScheduling ClusterSchedulingSpec `json:"clusterScheduling,omitempty"`
+	DispatchingWindow DispatchingWindowSpec `json:"dispatchingWindow,omitempty"`
+	RuntimeDuration   RuntimeDurationSpec   `json:"runtimeDuration,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
