4e8c15a4f3bfe00bfb4ae14913819ad49470409e
Laurentiu Bradin <laurentiu.bradin@ibm.com>
2023-04-04 17:07:00 +0300

    Upgraded the golang version Updated travis config for the new version Updated build.sh scripts for the new version

--- deployment/build.sh
+++ deployment/build.sh
@@ -4,6 +4,6 @@ set -x
 project_root=$(cd ..; pwd)
 git_path=github.com/project-codeflare/multi-cluster-app-dispatcher
 
-container_id=$(docker run  --rm -v "$project_root":/go/src/$git_path -d -w /go/src/$git_path/deployment golang:1.16.3-alpine3.13 ./build-inside-container.sh)
+container_id=$(docker run  --rm -v "$project_root":/go/src/$git_path -d -w /go/src/$git_path/deployment golang:1.19 ./build-inside-container.sh)
 
 docker logs -f $container_id
