b8bf6419556fc002284859c6d1c9c45d7f9feb23
Laurentiu Bradin <laurentiu.bradin@ibm.com>
2023-04-05 10:02:05 +0000

    e2e test script updates

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -27,13 +27,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
+export ROOT_DIR="$(dirname "$(dirname "$(readlink -fn "$0")")")"
 export LOG_LEVEL=3
-export CLEANUP_CLUSTER=${CLEANUP_CLUSTER:-1}
+export CLEANUP_CLUSTER=${CLEANUP_CLUSTER:-"true"}
 export CLUSTER_CONTEXT="--name test"
 # Using older image due to older version of kubernetes cluster"
-export IMAGE_NGINX="nginx:1.15.12"
-export IMAGE_ECHOSERVER="k8s.gcr.io/echoserver:1.4"
+export IMAGE_ECHOSERVER="kicbase/echo-server:1.0"
+export IMAGE_UBUNTU_LATEST="ubuntu:latest"
 export KIND_OPT=${KIND_OPT:=" --config ${ROOT_DIR}/hack/e2e-kind-config.yaml"}
 export KA_BIN=_output/bin
 export WAIT_TIME="20s"
@@ -41,30 +41,40 @@ export IMAGE_REPOSITORY_MCAD="${1}"
 export IMAGE_TAG_MCAD="${2}"
 export MCAD_IMAGE_PULL_POLICY="${3-Always}"
 export IMAGE_MCAD="${IMAGE_REPOSITORY_MCAD}:${IMAGE_TAG_MCAD}"
+CLUSTER_STARTED="false"
 export KUTTL_VERSION=0.15.0
 export KUTTL_TEST_OPT="--config ${ROOT_DIR}/kuttl-test.yaml"
 # FOR DEBUGGING
 #export KUTTL_TEST_OPT="--config ${ROOT_DIR}/kuttl-test.yaml --skip-delete"
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-# Using older version due to older version of kubernetes cluster"
-sudo apt-get install -y --allow-unauthenticated kubectl=1.17.0-00
+function update_test_host {
+  sudo apt-get update && sudo apt-get install -y apt-transport-https curl 
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+  sudo apt-get update
+  # Using older version due to older version of kubernetes cluster"
+  sudo apt-get install -y --allow-unauthenticated kubectl=1.17.0-00
 
-# Download kind binary (0.6.1)
-sudo curl -o /usr/local/bin/kind -L https://github.com/kubernetes-sigs/kind/releases/download/v0.11.0/kind-linux-amd64
-sudo chmod +x /usr/local/bin/kind
+  # Download kind binary (0.6.1)
+  sudo curl -o /usr/local/bin/kind -L https://github.com/kubernetes-sigs/kind/releases/download/v0.11.0/kind-linux-amd64
+  sudo chmod +x /usr/local/bin/kind
 
-# Download kuttl plugin
-sudo curl -sSLf --output /tmp/kubectl-kuttl https://github.com/kudobuilder/kuttl/releases/download/v${KUTTL_VERSION}/kubectl-kuttl_${KUTTL_VERSION}_linux_x86_64 && sudo mv /tmp/kubectl-kuttl /usr/local/bin && sudo chmod a+x /usr/local/bin/kubectl-kuttl
+  # Installing helm3
+  curl -fsSL -o ${ROOT_DIR}/get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+  chmod 700 ${ROOT_DIR}/get_helm.sh
+  ${ROOT_DIR}/get_helm.sh
+  sleep 10
+  
+  # Download kuttl plugin
+  sudo curl -sSLf --output /tmp/kubectl-kuttl https://github.com/kudobuilder/kuttl/releases/download/v${KUTTL_VERSION}/kubectl-kuttl_${KUTTL_VERSION}_linux_x86_64 && sudo mv /tmp/kubectl-kuttl /usr/local/bin && sudo chmod a+x /usr/local/bin/kubectl-kuttl
 
-# check if kind installed
+}
+
+# check if pre-requizites are installed.
 function check-prerequisites {
   echo "checking prerequisites"
   which kind >/dev/null 2>&1
-  if [[ $? -ne 0 ]]; then
+  if [ $? -ne 0 ]; then
     echo "kind not installed, exiting."
     exit 1
   else
@@ -72,21 +82,20 @@ function check-prerequisites {
   fi
 
   which kubectl >/dev/null 2>&1
-  if [[ $? -ne 0 ]]; then
+  if [ $? -ne 0 ]; then
     echo "kubectl not installed, exiting."
     exit 1
   else
-    echo -n "found kubectl, " && kubectl version --short --client
+    echo -n "found kubectl, " && kubectl version 
   fi
-
   kubectl kuttl version >/dev/null 2>&1
-  if [[ $? -ne 0 ]]; then
+  if [ $? -ne 0 ]; then
     echo "kuttl plugin for kubectl not installed, exiting."
     exit 1
   else
     echo -n "found kuttl plugin for kubectl, " && kubectl kuttl version
   fi
-
+  
   if [[ $IMAGE_REPOSITORY_MCAD == "" ]]
   then
     echo "No MCAD image was provided."
@@ -96,34 +105,75 @@ function check-prerequisites {
     echo "No MCAD image tag was provided for: ${IMAGE_REPOSITORY_MCAD}."
     exit 1
   else
-    echo -n "end to end test with ${IMAGE_MCAD}."
+    echo "end to end test with ${IMAGE_MCAD}."
   fi
+  
+  which helm >/dev/null 2>&1
+  if [ $? -ne 0 ]
+  then
+    echo "helm not installed, exiting."
+    exit 1
+  else
+    echo -n "found helm, " && helm version --short
+  fi  
+
 }
 
 function kind-up-cluster {
-  check-prerequisites
   echo "Running kind: [kind create cluster ${CLUSTER_CONTEXT} ${KIND_OPT}]"
   kind create cluster ${CLUSTER_CONTEXT} ${KIND_OPT} --wait ${WAIT_TIME}
+  if [ $? -ne 0 ]
+  then
+    echo "Failed to start kind cluster"
+    exit 1
+  fi
+  CLUSTER_STARTED="true"
 
-  docker images
-  docker pull ${IMAGE_ECHOSERVER}
-  docker pull ${IMAGE_NGINX}
+  docker pull ${IMAGE_ECHOSERVER} 
+  if [ $? -ne 0 ]
+  then
+    echo "Failed to pull ${IMAGE_ECHOSERVER}"
+    exit 1
+  fi
+
+  docker pull ${IMAGE_UBUNTU_LATEST}
+  if [ $? -ne 0 ]
+  then
+    echo "Failed to pull ${IMAGE_UBUNTU_LATEST}"
+    exit 1
+  fi
+
   if [[ "$MCAD_IMAGE_PULL_POLICY" = "Always" ]]
   then
     docker pull ${IMAGE_MCAD}
+    if [ $? -ne 0 ]
+    then
+      echo "Failed to pull ${IMAGE_MCAD}"
+      exit 1
+    fi
   fi
   docker images
-  
-  kind load docker-image ${IMAGE_NGINX} ${CLUSTER_CONTEXT}
-  kind load docker-image ${IMAGE_ECHOSERVER} ${CLUSTER_CONTEXT}
-  kind load docker-image ${IMAGE_MCAD} ${CLUSTER_CONTEXT}
+
+  for image in ${IMAGE_ECHOSERVER} ${IMAGE_UBUNTU_LATEST} ${IMAGE_MCAD}
+  do
+    kind load docker-image ${image} ${CLUSTER_CONTEXT}
+    if [ $? -ne 0 ]
+    then
+      echo "Failed to load image ${image} in cluster"
+      exit 1
+    fi
+  done 
 }
 
 # clean up
 function cleanup {
     echo "==========================>>>>> Cleaning up... <<<<<=========================="
     echo " "
-
+    if [[ ${CLUSTER_STARTED} == "false" ]]
+    then
+      echo "Cluster was not started, nothing more to do."
+      return
+    fi  
 
     echo "Custom Resource Definitions..."
     echo "kubectl get crds"
@@ -160,10 +210,19 @@ function cleanup {
     echo "===================================================================================="
     echo "==========================>>>>> MCAD Controller Logs <<<<<=========================="
     echo "===================================================================================="
-    echo "kubectl logs ${mcad_pod} -n kube-system"
-    kubectl logs ${mcad_pod} -n kube-system
-
-    kind delete cluster ${CLUSTER_CONTEXT}
+    local mcad_pod=$(kubectl get pods -n kube-system | grep mcad-controller | awk '{print $1}')
+    if [[ "$mcad_pod" != "" ]]
+    then
+      echo "kubectl logs ${mcad_pod} -n kube-system"
+      kubectl logs ${mcad_pod} -n kube-system
+    fi
+    rm -rf ${ROOT_DIR}/get_helm.sh
+    if [[ $CLEANUP_CLUSTER == "true" ]]
+    then
+      kind delete cluster ${CLUSTER_CONTEXT}     
+    else 
+      echo "Cluster requested to stay up, not deleting cluster"     
+    fi 
 }
 
 debug_function() {
@@ -208,7 +267,7 @@ spec:
             spec:
               containers:
                - name: hellodiana-2-test-0
-                 image: k8s.gcr.io/echoserver:1.4
+                 image: ${IMAGE_ECHOSERVER}
                  imagePullPolicy: Always
                  ports:
                  - containerPort: 80
@@ -264,60 +323,39 @@ EOF
 }
 
 function kube-test-env-up {
-    cd ${ROOT_DIR}
+  # Hack to setup for 'go test' call which expects this path.
+  if [ ! -f $HOME/.kube/config ]
+  then
+    echo "'$HOME/.kube/config' not found"
+    exit 1
+  fi
 
-    echo "---"
-    export KUBECONFIG="$(kind get kubeconfig-path ${CLUSTER_CONTEXT})"
+  echo "---"
+  echo "kubectl version"
+  kubectl version
 
-    echo "---"
-    echo "KUBECONFIG file: ${KUBECONFIG}"
+  echo "---"
+  echo "kubectl config current-context"
+  kubectl config current-context
 
-    echo "---"
-    echo "kubectl version"
-    kubectl version
+  echo "---"
+  echo "kubectl get nodes"
+  kubectl get nodes -o wide
 
-    echo "---"
-    echo "kubectl config current-context"
-    kubectl config current-context
+  echo "Installing Podgroup CRD"
+  kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/scheduler-plugins/277b6bdec18f8a9e9ccd1bfeaf4b66495bfc6f92/config/crd/bases/scheduling.sigs.k8s.io_podgroups.yaml
 
-    echo "---"
-    echo "kubectl get nodes"
-    kubectl get nodes -o wide
+  helm version
 
-    # Hack to setup for 'go test' call which expects this path.
-    if [ ! -z $HOME/.kube/config ]
-    then
-      cp $KUBECONFIG $HOME/.kube/config
-
-      echo "---"
-      cat $HOME/.kube/config
-    fi
-
-    # Installing helm3
-
-    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-    chmod 700 get_helm.sh
-    ./get_helm.sh
-    sleep 10
-
-    helm version 
-
-    echo "Installing Podgroup CRD"
-
-    helm version
-
-    # Turn off master taints
-    kubectl taint nodes --all node-role.kubernetes.io/master-
-
+  # Turn off master taints
+  kubectl taint nodes --all node-role.kubernetes.io/master-
 }
 
 function mcad-quota-management-up {
-    cd $ROOT_DIR/deployment/mcad-controller
-
     # start mcad controller
     echo "Starting MCAD Controller for Quota Management Testing..."
     echo "helm upgrade --install mcad-controller . namespace kube-system wait set loglevel=10 set resources.requests.cpu=1000m set resources.requests.memory=1024Mi set resources.limits.cpu=4000m set resources.limits.memory=4096Mi set image.repository=$IMAGE_REPOSITORY_MCAD set image.tag=$IMAGE_TAG_MCAD set image.pullPolicy=$MCAD_IMAGE_PULL_POLICY set configMap.quotaEnabled='true' set quotaManagement.rbac.apiGroup=ibm.com set quotaManagement.rbac.resource=quotasubtrees set configMap.name=mcad-controller-configmap set configMap.preemptionEnabled='true'"
-    helm upgrade --install mcad-controller .  --namespace kube-system --wait --set loglevel=10 --set resources.requests.cpu=1000m --set resources.requests.memory=1024Mi --set resources.limits.cpu=4000m --set resources.limits.memory=4096Mi --set image.repository=$IMAGE_REPOSITORY_MCAD --set image.tag=$IMAGE_TAG_MCAD --set image.pullPolicy=$MCAD_IMAGE_PULL_POLICY --set configMap.quotaEnabled='"true"' --set quotaManagement.rbac.apiGroup=ibm.com --set quotaManagement.rbac.resource=quotasubtrees --set configMap.name=mcad-controller-configmap --set configMap.preemptionEnabled='"true"'
+    helm upgrade --install mcad-controller ${ROOT_DIR}/deployment/mcad-controller  --namespace kube-system --wait --set loglevel=10 --set resources.requests.cpu=1000m --set resources.requests.memory=1024Mi --set resources.limits.cpu=4000m --set resources.limits.memory=4096Mi --set image.repository=$IMAGE_REPOSITORY_MCAD --set image.tag=$IMAGE_TAG_MCAD --set image.pullPolicy=$MCAD_IMAGE_PULL_POLICY --set configMap.quotaEnabled='"true"' --set quotaManagement.rbac.apiGroup=ibm.com --set quotaManagement.rbac.resource=quotasubtrees --set configMap.name=mcad-controller-configmap --set configMap.preemptionEnabled='"true"'
     sleep 10
 }
 
@@ -334,28 +372,15 @@ function mcad-quota-management-down {
 }
 
 function mcad-up {
-    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/scheduler-plugins/277b6bdec18f8a9e9ccd1bfeaf4b66495bfc6f92/config/crd/bases/scheduling.sigs.k8s.io_podgroups.yaml
-
-    cd $ROOT_DIR/deployment/mcad-controller
-
     # start mcad controller
     echo "Starting MCAD Controller..."
     echo "helm install mcad-controller namespace kube-system wait set loglevel=2 set resources.requests.cpu=1000m set resources.requests.memory=1024Mi set resources.limits.cpu=4000m set resources.limits.memory=4096Mi set image.repository=$IMAGE_REPOSITORY_MCAD set image.tag=$IMAGE_TAG_MCAD set image.pullPolicy=$MCAD_IMAGE_PULL_POLICY"
-    helm upgrade --install mcad-controller .  --namespace kube-system --wait --set loglevel=2 --set resources.requests.cpu=1000m --set resources.requests.memory=1024Mi --set resources.limits.cpu=4000m --set resources.limits.memory=4096Mi --set configMap.name=mcad-controller-configmap --set configMap.podCreationTimeout='"120000"' --set configMap.quotaEnabled='"false"' --set coscheduler.rbac.apiGroup=scheduling.sigs.k8s.io --set coscheduler.rbac.resource=podgroups --set image.repository=$IMAGE_REPOSITORY_MCAD --set image.tag=$IMAGE_TAG_MCAD --set image.pullPolicy=$MCAD_IMAGE_PULL_POLICY
+    helm upgrade --install mcad-controller ${ROOT_DIR}/deployment/mcad-controller  --namespace kube-system --wait --set loglevel=2 --set resources.requests.cpu=1000m --set resources.requests.memory=1024Mi --set resources.limits.cpu=4000m --set resources.limits.memory=4096Mi --set configMap.name=mcad-controller-configmap --set configMap.podCreationTimeout='"120000"' --set configMap.quotaEnabled='"false"' --set coscheduler.rbac.apiGroup=scheduling.sigs.k8s.io --set coscheduler.rbac.resource=podgroups --set image.repository=$IMAGE_REPOSITORY_MCAD --set image.tag=$IMAGE_TAG_MCAD --set image.pullPolicy=$MCAD_IMAGE_PULL_POLICY
 
     sleep 10
 }
 
 function mcad-env-status {
-    echo "Listing MCAD Controller Helm Chart and Pod YAML..."
-    helm list
-    mcad_pod=$(kubectl get pods -n kube-system | grep mcad-controller | awk '{print $1}')
-    if [[ "$mcad_pod" != "" ]]
-    then
-        kubectl get pod ${mcad_pod} -n kube-system -o yaml
-    fi
-
-    sleep 10
     echo "Listing MCAD Controller Helm Chart and Pod YAML..."
     helm list
     mcad_pod=$(kubectl get pods -n kube-system | grep mcad-controller | awk '{print $1}')
@@ -385,9 +410,6 @@ function mcad-env-status {
 }
 
 function kuttl-tests {
-  mcad-quota-management-up
-  mcad-env-status
-  cd ${ROOT_DIR}
   echo "==============>>>>> Running Quota Management Kuttl E2E tests... <<<<<=============="
   echo "kubectl kuttl test ${KUTTL_TEST_OPT}"
   kubectl kuttl test ${KUTTL_TEST_OPT}
@@ -398,28 +420,23 @@ function kuttl-tests {
     # Takes a bit of time for namespace created in kuttl testing to completely delete.
     sleep 40
   fi
-  mcad-quota-management-down
 }
 
 trap cleanup EXIT
 
+#Only run this function if we are running on the test build machinbe,
+#currently  ubuntu 16.04 xenial
+if [ "$(lsb_release -c -s 2>&1 | grep bullseye)" == "bullseye" ]; then 
+   update_test_host
+fi
+check-prerequisites 
 kind-up-cluster
-
 kube-test-env-up
-
-###
-# Quota management testing
-###
-kuttl-tests
-
-###
-# Non-quota management testing
-###
-mcad-up
-
+mcad-quota-management-up
 mcad-env-status
-
-cd ${ROOT_DIR}
-
+kuttl-tests
+mcad-quota-management-down
+mcad_up
+mcad-env-status
 echo "==========================>>>>> Running E2E tests... <<<<<=========================="
-go test ./test/e2e -v -timeout 55m
+go test ./test/e2e -v -timeout 75m
