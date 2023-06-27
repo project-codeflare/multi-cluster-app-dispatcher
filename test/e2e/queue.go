ef5c6a871beeff82108a0dad4de3831e2630f27e
Laurentiu Bradin <109964136+z103cb@users.noreply.github.com>
2023-06-27 15:38:29 +0300

    Removed focused test.

--- test/e2e/queue.go
+++ test/e2e/queue.go
@@ -414,7 +414,7 @@ var _ = Describe("AppWrapper E2E Test", func() {
 		Expect(err).NotTo(HaveOccurred())
 	})
 
-	FIt("MCAD Scheduling Fail Fast Preemption Test", func() {
+	It("MCAD Scheduling Fail Fast Preemption Test", func() {
 		fmt.Fprintf(os.Stdout, "[e2e] MCAD Scheduling Fail Fast Preemption Test - Started.\n")
 
 		context := initTestContext()
