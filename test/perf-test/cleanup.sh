a43c26be1ea08606c7025969a603c374e757553f
Vishakha Ramani <vishakharamani@Vishakhas-MacBook-Pro.local>
2023-07-10 11:42:26 -0400

    summer-tests

--- test/perf-test/cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `kubectl get appwrapper -n default |grep defaultaw | awk '{print $1}'`; do kubectl delete appwrapper $i -n default ; done
--- test/perf-test/nomcad-cleanup-kwok.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `kubectl get job -n default |grep noaw-kwok-job | awk '{print $1}'`; do kubectl delete job $i -n default ; done
