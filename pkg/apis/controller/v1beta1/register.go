46cd63f9680e7aee33b7f1c54b7d06378fda14f7
Antonin Stefanutti <antonin@stefanutti.fr>
2023-08-03 11:06:24 +0200

    api: Remove QueueJob API

--- pkg/apis/controller/v1beta1/register.go
+++ pkg/apis/controller/v1beta1/register.go
@@ -1,19 +1,4 @@
 /*
-Copyright 2017 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-/*
 Copyright 2019, 2021 The Multi-Cluster App Dispatcher Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,10 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package v1beta1
 
 import (
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -48,14 +33,6 @@ const GroupName = "mcad.ibm.com"
 // SchemeGroupVersion is the group version used to register these objects.
 var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1beta1"}
 
-var Versions = []apiextensionsv1.CustomResourceDefinitionVersion{
-	{
-		Name:    SchemeGroupVersion.Version,
-		Served:  true,
-		Storage: true,
-	},
-}
-
 // Resource takes an unqualified resource and returns a Group-qualified GroupResource.
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
@@ -66,8 +43,6 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&SchedulingSpec{},
 		&SchedulingSpecList{},
-		&QueueJob{},
-		&QueueJobList{},
 		&AppWrapper{},
 		&AppWrapperList{},
 	)
--- pkg/client/queuejob.go
+++ pkg/client/queuejob.go
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package client
 
 import (
@@ -32,61 +33,8 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-const QueueJobCrdPath = "config/crd/bases/mcad.ibm.com_queuejobs.yaml"
-const queueJobKindName = arbv1.QueueJobPlural + "." + arbv1.GroupName
-
 const AppWrapperCrdPath = "config/crd/bases/mcad.ibm.com_appwrappers.yaml"
 const appWrapperKindName = arbv1.AppWrapperPlural + "." + arbv1.GroupName
-
-func CreateQueueJobKind(clientset apiextensionsclient.Interface) (*apiextensionsv1.CustomResourceDefinition, error) {
-	f, err := os.ReadFile(QueueJobCrdPath)
-	if err != nil {
-		return nil, fmt.Errorf("cannot load the CRD from file %s: %v", QueueJobCrdPath, err)
-	}
-
-	crd := &apiextensionsv1.CustomResourceDefinition{}
-	err = yaml.Unmarshal(f, crd)
-	if err != nil {
-		return nil, fmt.Errorf("cannot unmarshal the CRD: %v", err)
-	}
-
-	_, err = clientset.ApiextensionsV1().CustomResourceDefinitions().Create(context.Background(), crd, metav1.CreateOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	// wait for CRD being established
-	err = wait.Poll(500*time.Millisecond, 60*time.Second, func() (bool, error) {
-		crd, err = clientset.ApiextensionsV1().CustomResourceDefinitions().Get(context.Background(), queueJobKindName, metav1.GetOptions{})
-		if err != nil {
-			return false, err
-		}
-		for _, cond := range crd.Status.Conditions {
-			switch cond.Type {
-			case apiextensionsv1.Established:
-				if cond.Status == apiextensionsv1.ConditionTrue {
-					return true, err
-				}
-			case apiextensionsv1.NamesAccepted:
-				if cond.Status == apiextensionsv1.ConditionFalse {
-					fmt.Printf("Name conflict: %v\n", cond.Reason)
-				}
-			}
-		}
-		return false, err
-	})
-	if err != nil {
-		deleteErr := clientset.ApiextensionsV1().CustomResourceDefinitions().Delete(context.Background(), queueJobKindName, metav1.DeleteOptions{})
-		if deleteErr != nil {
-			return nil, errors.NewAggregate([]error{err, deleteErr})
-		}
-		return nil, err
-	}
-
-	klog.V(4).Infof("QueueJob CRD was created.")
-
-	return crd, nil
-}
 
 func CreateAppWrapperKind(clientset apiextensionsclient.Interface) (*apiextensionsv1.CustomResourceDefinition, error) {
 	f, err := os.ReadFile(AppWrapperCrdPath)
