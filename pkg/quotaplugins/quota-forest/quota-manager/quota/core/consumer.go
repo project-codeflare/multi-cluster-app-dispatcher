82e3233eccd718c9233b8351fc208a12c1693c88
tantawi <tantawi@us.ibm.com>
2023-08-10 17:47:15 -0400

    add and fix try and undo allocate testing

--- pkg/quotaplugins/quota-forest/quota-manager/quota/core/consumer.go
+++ pkg/quotaplugins/quota-forest/quota-manager/quota/core/consumer.go
@@ -1,11 +1,11 @@
 /*
-Copyright 2022 The Multi-Cluster App Dispatcher Authors.
+Copyright 2022, 2023 The Multi-Cluster App Dispatcher Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -118,6 +118,13 @@ func (c *Consumer) SetNode(aNode *QuotaNode) {
 func (c *Consumer) IsAllocated() bool {
 	return c.aNode != nil
 }
+
+// ByID implements sort.Interface based on the ID field.
+type ByID []*Consumer
+
+func (c ByID) Len() int           { return len(c) }
+func (c ByID) Less(i, j int) bool { return c[i].id < c[j].id }
+func (c ByID) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
 
 // String : a print out of the consumer
 func (c *Consumer) String() string {
