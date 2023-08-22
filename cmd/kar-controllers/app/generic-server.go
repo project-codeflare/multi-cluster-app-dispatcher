7941a94d84a2b4c1475f5a6ee7e6cfddec13a682
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2023-08-22 12:36:27 +0100

    refactor: add better error and use custom timeout

--- cmd/kar-controllers/app/generic-server.go
+++ cmd/kar-controllers/app/generic-server.go
@@ -13,21 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-/*
-Copyright 2019, 2021 The Multi-Cluster App Dispatcher Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package app
 
 import (
@@ -109,9 +94,9 @@ func (s *Server) Shutdown() error {
 		return nil
 	}
 	
-	logger.Info("Stopping server")
+	logger.Infof("Shutting down endpoint %s at %s (gracefully waiting for %s)", s.endpoint, s.listener.Addr(), s.shutdownTimeout)
 
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
 	defer cancel()
 
 	// Try graceful shutdown
