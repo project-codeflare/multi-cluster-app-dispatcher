4d7ab51a4b7774ae18c7884c94b67e9841cf1c9d
Jaroslaw Cwiklik <cwiklik@wecm-9-67-132-200.wecm.ibm.com>
2023-09-24 10:13:47 -0400

    added TODO

--- pkg/controller/queuejob/queuejob_controller_ex.go
+++ pkg/controller/queuejob/queuejob_controller_ex.go
@@ -2270,9 +2270,10 @@ func (cc *XController) manageQueueJob(qj *arbv1.AppWrapper, podPhaseChanges bool
 				klog.V(10).Infof("[Dispatcher Controller] Dispatched AppWrapper %s to Agent ID: %s.", qj.Name, agentId)
 				if cc.serverOption.ExternalDispatch {
 					values := strings.Split(agentId, "/")
-					klog.V(10).Infof("[Dispatcher Controller] ... Dispatching AppWrapper %s to Agent ID: %s Through External Dispatcher.", qj.Name, values[len(values)-1])					
+					klog.V(10).Infof("[Dispatcher Controller] Dispatching AppWrapper %s to Agent ID: %s Through External Dispatcher.", qj.Name, values[len(values)-1])					
 					qj.Spec.SchedSpec.ClusterScheduling.PolicyResult = arbv1.ClusterDecision {
 						TargetCluster: arbv1.ClusterReference{
+							// TODO check if values len > 0.
 							Name: values[len(values)-1],
 						}, 
 						PolicySource : []arbv1.PolicySourceReference {
