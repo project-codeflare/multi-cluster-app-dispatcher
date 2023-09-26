c74a7ec1b976a23e8eb73ffc5904d63230a7d24b
Antonin Stefanutti <antonin@stefanutti.fr>
2023-09-26 09:44:15 +0200

    Upgrade code-generator to version v0.27.2

--- pkg/client/clientset/versioned/typed/controller/v1beta1/fake/fake_appwrapper.go
+++ pkg/client/clientset/versioned/typed/controller/v1beta1/fake/fake_appwrapper.go
@@ -24,7 +24,6 @@ import (
 	v1beta1 "github.com/project-codeflare/multi-cluster-app-dispatcher/pkg/apis/controller/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -36,9 +35,9 @@ type FakeAppWrappers struct {
 	ns   string
 }
 
-var appwrappersResource = schema.GroupVersionResource{Group: "workload.codeflare.dev", Version: "v1beta1", Resource: "appwrappers"}
+var appwrappersResource = v1beta1.SchemeGroupVersion.WithResource("appwrappers")
 
-var appwrappersKind = schema.GroupVersionKind{Group: "workload.codeflare.dev", Version: "v1beta1", Kind: "AppWrapper"}
+var appwrappersKind = v1beta1.SchemeGroupVersion.WithKind("AppWrapper")
 
 // Get takes name of the appWrapper, and returns the corresponding appWrapper object, and an error if there is any.
 func (c *FakeAppWrappers) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1beta1.AppWrapper, err error) {
@@ -117,7 +116,7 @@ func (c *FakeAppWrappers) UpdateStatus(ctx context.Context, appWrapper *v1beta1.
 // Delete takes name of the appWrapper and deletes it. Returns an error if one occurs.
 func (c *FakeAppWrappers) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	_, err := c.Fake.
-		Invokes(testing.NewDeleteAction(appwrappersResource, c.ns, name), &v1beta1.AppWrapper{})
+		Invokes(testing.NewDeleteActionWithOptions(appwrappersResource, c.ns, name, opts), &v1beta1.AppWrapper{})
 
 	return err
 }
